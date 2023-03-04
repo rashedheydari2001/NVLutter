08ecfbbab1ad2c7f2b890a71684921494a4fb038
rashedheydari2001 <81459372+rashedheydari2001@users.noreply.github.com>
2023-03-04 20:53:49 +0330

    Update theme.lua

--- lua/custom/theme.lua
+++ lua/custom/theme.lua
@@ -2,11 +2,11 @@ local set = vim.opt
 local vimscript = vim.cmd
 
 -- chack found themes
-local themes_status = pcall(require, "onedark")
+local themes_status = pcall(require, "gruvbox")
 
 -- auto load theme
 if themes_status then
-	require("colors.onedark")
+	require("colors.gruvbox")
 end
 
 set.termguicolors = true
