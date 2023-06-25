3e15e9b7849c876d87fd9309009bf845abf6ab1f
rashed <rashedheydari.2001@gmail.com>
2023-06-25 12:55:19 +0330

    python has been fixed

---
+++ lua/pylsp.lua
@@ -0,0 +1,6 @@
+vim.g.LanguageClient_serverCommands = {
+  python = { "pyls" }
+}
+vim.g.LanguageClient_autoStart = 1
+vim.g.LanguageClient_serverLogMessageLevel = "warn"
+vim.g.LanguageClient_loggingLevel = "WARN"
