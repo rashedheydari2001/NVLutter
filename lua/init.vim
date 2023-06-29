0e67f7d8a289a9ebd8e0509f4eb019e8a05af995
rashed <rashedheydari.2001@gmail.com>
2023-06-29 14:55:16 +0330

    the code hase been fixed

--- lua/init.vim
+++
@@ -1,4 +0,0 @@
-let g:LanguageClient_serverCommands = {'python': ['pyls']}
-let g:LanguageClient_autoStart = 1
-let g:LanguageClient_serverLogMessageLevel = 'warn'
-let g:LanguageClient_loggingLevel = 'WARN'
--- lua/keyboard.lua
+++ lua/keyboard.lua
@@ -63,14 +63,6 @@ cmp.setup({
 
 --prefix name setting
 local wk = require("which-key")
--- As an example, we will create the following mappings:
---  * <leader>ff find files
---  * <leader>fr show recent files
---  * <leader>fb Foobar
--- we'll document:
---  * <leader>fn new file
---  * <leader>fe edit file
--- and hide <leader>1
 
 wk.register({
   f = {
